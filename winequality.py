ace47e83c1ade1c4777481d9c3d17ad62f711434
Roy Bhattacharya <anroybhattacharya@w16408.wireless.davidson.edu>
2018-01-26 18:16:56 -0500

    Add train-validate-test split

--- winequality.py
+++ winequality.py
@@ -9,7 +9,7 @@ def get_preprocessed_dataset(filename):
     df = read_file(filename)
     standardize(df)
     df = shuffle_data(df)
-    return (df.loc[:, 'fixed acidity':'alcohol'], df['quality'])
+    return split(df)
 
 """
 Function to read the input csv file and return the corresponding dataframe.
@@ -33,3 +33,12 @@ Function that shuffles the dataset.
 """
 def shuffle_data(df):
     return df.reindex(np.random.permutation(df.index))
+
+"""
+Function that splits the dataset into a 60:20:20 split.
+"""
+def split(df):
+    train, validate, test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
+    return (train.loc[:, 'fixed acidity':'alcohol'], train['quality'],
+        validate.loc[:, 'fixed acidity':'alcohol'],
+        test.loc[:, 'fixed acidity':'alcohol'])
